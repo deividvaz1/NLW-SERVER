159ab93d45567bd7521200675e0aae238746cf76
deividvaz1 <deividvaz100@gmail.com>
2023-05-31 13:59:12 -0300

    finalizado

---
+++ api/serverless.js
@@ -0,0 +1,11 @@
+import * as dotenv from 'dotenv'
+
+// Require the framework
+import { init } from '../src/server'
+dotenv.config()
+
+export default async (req, res) => {
+  const app = init()
+  await app.ready()
+  app.server.emit('request', req, res)
+}
