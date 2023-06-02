67a6c7c00da839c4ef22bf1b66b08b705ec57c98
deividvaz1 <deividvaz100@gmail.com>
2023-06-02 12:33:34 -0300

    teste

--- src/server.ts
+++ src/server.ts
@@ -15,7 +15,7 @@ export function init() {
   })
 
   app.register(cors, {
-    origin: ['https://nlw-artur.vercel.app', 'http://localhost:3000'],
+    origin: ['https://projeto-nlw-five.vercel.app/', 'http://localhost:3000'],
   })
 
   app.register(multipart)
